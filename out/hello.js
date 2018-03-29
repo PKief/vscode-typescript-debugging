778af6b687f5a413f15c0009e78b03bda4e671ad
PKief <philipp.kief@gmx.de>
2018-03-29 08:57:35 +0200

    Removed out-folder from version control

--- out/hello.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-/** Say hello */
-exports.hello = function (name) {
-    var greeting = "Hello " + name + "!";
-    return greeting;
-};
-//# sourceMappingURL=hello.js.map
